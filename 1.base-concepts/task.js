69e9d0a8f0e1b12028b92e7d2f8101e52df91e7d
Klein Kristina <klein281293@gmail.com>
2022-04-16 20:30:48 +0700

    homework task 1.1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,20 @@
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  'use strict';
+  let dis = Math.pow(b, 2) - (4 * a * c);
+  if (dis < 0) {
+      return [];
+  }
+  if (dis == 0) {
+      return [
+          -b / (2 * a)
+      ];
+  }
+  if (dis > 0) {
+      return [
+          (-b + Math.sqrt(dis)) / (2 * a),
+          (-b - Math.sqrt(dis)) / (2 * a)
+      ];
+  }
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
