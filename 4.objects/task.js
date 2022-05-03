7f7d682a4152118a28a1c784a87befd7a2867e3a
Klein Kristina <klein281293@gmail.com>
2022-05-03 21:10:49 +0700

    homework task 4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,43 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [mark];
+  } else {
+    this.marks.push(mark);
+  }
+}
+
+Student.prototype.addMarks = function () {
+  if (arguments.length) {
+    for (i = 0; i < arguments.length; i++) {
+      this.addMark(arguments[i]);
+    }
+  }
+}
+
+Student.prototype.getAverage = function () {
+  let sum = 0,
+      avg = 0;
+  if (this.marks !== undefined) {
+    for (i = 0; i < this.marks.length; i++) {
+      sum += this.marks[i];
+    }
+    avg = sum / this.marks.length;
+  }
+  return avg;
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
